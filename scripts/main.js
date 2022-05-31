6a5c45c61f88a2774631d764717495e6e5393843
DailyGrow <858519155@qq.com>
2022-05-30 17:17:17 -0700

    add date/time display

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,53 @@ function ThemeSwitch(){
     }
     
 }
+
+var choice=document.querySelector('#CT Display');
+
+
+function showtime(){
+    var refresh=1000;
+    
+    mytime=setTimeout('display_ct()',refresh);
+}
+
+function display_ct(){
+    
+    var x=new Date();
+    var str=document.querySelector('input[name="radioButton"]:checked').value;
+    var res="";
+    var s=x.toString();
+    if(str=='Date'){
+        var arr=s.split(" ");
+        res=arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3];
+        
+    }
+    else{
+        var arr=s.split(" ");
+        
+        
+        var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+        res=x.getHours()+":"+x.getMinutes()+":"+x.getSeconds()+ampm;
+    }
+
+    var idObject=document.getElementById("oldinput");
+    if(idObject==null){
+        let input=document.createElement("input");
+        input.setAttribute("type","text");
+        input.setAttribute("id","oldinput");
+        input.value=res;
+        //input.style.width="px";
+        input.style.height="20px";
+        input.style.marginLeft="20px";
+    
+        let currDiv=document.getElementById("CT-Display");
+    //currDiv.insertAdjacentHTML('afterend',newDiv);
+    
+        currDiv.appendChild(input);
+    }
+    else{
+        idObject.value=res;
+    }
+    
+    
+}
